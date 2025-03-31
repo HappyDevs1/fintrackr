588a0ff7186407ab8a966e620efa4cfe007ffbbf
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2025-03-31 13:57:37 +0200

    feat: :sparkles: adding a child process to send data to the model for prediction

--- server/index.ts
+++ server/index.ts
@@ -5,11 +5,14 @@ import { getAccounts } from "./controllers/accounts";
 // Have to fix the error for this import
 // import { getAccountBalance } from "./controllers/accounts";
 import { getTransactions } from "./controllers/transactions";
+import { spawn } from 'child_process';
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
+
+const modelPath = "./model/predictModel.py";
 
 app.use(express.json());
 
@@ -45,6 +48,34 @@ app.use('/transactions', async (req: Request, res: Response) => {
     res.status(500).json({ error: error.message });
   }
 }); // Transactions route
+app.get('/forecast', async (req: Request, res: Response) => {
+  try {
+    const transactions = await getTransactions();
+    res.status(200).json(transactions);
+    const pythonProcess = spawn('python', [modelPath,], {
+      stdio: ['pipe', 'pipe', 'pipe'],
+    });
+
+    pythonProcess.stdin.write(JSON.stringify(transactions));
+    pythonProcess.stdin.end();
+
+    let result = '';
+
+    pythonProcess.stdout.on('data', (data) => {
+      result += data.toString();
+    });
+
+    pythonProcess.on('close', (code) => {
+      if (code === 0) {
+        res.status(500).json({ forecast: result });
+      }
+      const { forecast, alerts } = JSON.parse(result);
+      res.json({ forecast, alerts});
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(PORT, () => { 
   console.log("Server running at PORT: ", PORT); 
